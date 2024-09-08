77688aa285802340b6f558563ef6b3d9720b6bd9
luispiedrahita258 <lepiedrahita5@misena.edu.co>
2024-09-07 19:30:53 -0500

    Aplicación de Películas

--- src/env.ts
+++ src/env.ts
@@ -1,35 +1,34 @@
 import { z } from "zod";
 
-export const env = createEnv({
-  server: {
-    DATABASE_URL: z.string().min(1),
-    APP_URL: z.string().url().min(1),
-    GOOGLE_SITE_VERIFICATION_ID: z.string().optional(),
-    GITHUB_ID: z.string().min(1),
-    GITHUB_SECRET: z.string().min(1),
-    NEXTAUTH_URL: z.string().url().optional(),
-    NEXTAUTH_SECRET: z.string().min(1),
-    STRIPE_SECRET_KEY: z.string().min(1),
-    STRIPE_WEBHOOK_SECRET_KEY: z.string().min(1),
-    STRIPE_SUBSCRIPTION_PRICE_ID: z.string().min(1),
-    TMDB_API_KEY: z.string().min(1),
-  },
-  client: {
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().min(1),
-    NEXT_PUBLIC_TMDB_API_KEY: z.string().min(1),
-  },
-  runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    APP_URL: process.env.APP_URL,
-    GOOGLE_SITE_VERIFICATION_ID: process.env.GOOGLE_SITE_VERIFICATION_ID,
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_WEBHOOK_SECRET_KEY: process.env.STRIPE_WEBHOOK_SECRET_KEY,
-    STRIPE_SUBSCRIPTION_PRICE_ID: process.env.STRIPE_SUBSCRIPTION_PRICE_ID,
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-  },
+// Define el esquema de validación para las variables de entorno
+const envSchema = z.object({
+  DATABASE_URL: z.string().min(1),
+  APP_URL: z.string().url().min(1),
+  GOOGLE_SITE_VERIFICATION_ID: z.string().optional(),
+  GITHUB_ID: z.string().min(1),
+  GITHUB_SECRET: z.string().min(1),
+  NEXTAUTH_URL: z.string().url().optional(),
+  NEXTAUTH_SECRET: z.string().min(1),
+  STRIPE_SECRET_KEY: z.string().min(1),
+  STRIPE_WEBHOOK_SECRET_KEY: z.string().min(1),
+  STRIPE_SUBSCRIPTION_PRICE_ID: z.string().min(1),
+  TMDB_API_KEY: z.string().min(1),
+  NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().min(1),
+  NEXT_PUBLIC_TMDB_API_KEY: z.string().min(1),
+});
+
+// Valida las variables de entorno
+export const env = envSchema.parse({
+  DATABASE_URL: process.env.DATABASE_URL,
+  APP_URL: process.env.APP_URL,
+  GOOGLE_SITE_VERIFICATION_ID: process.env.GOOGLE_SITE_VERIFICATION_ID,
+  GITHUB_ID: process.env.GITHUB_ID,
+  GITHUB_SECRET: process.env.GITHUB_SECRET,
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  STRIPE_WEBHOOK_SECRET_KEY: process.env.STRIPE_WEBHOOK_SECRET_KEY,
+  STRIPE_SUBSCRIPTION_PRICE_ID: process.env.STRIPE_SUBSCRIPTION_PRICE_ID,
+  NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  NEXT_PUBLIC_TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
 });
